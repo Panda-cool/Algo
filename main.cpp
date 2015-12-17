2e7aef967967e4f48c1f4988eae1d8fb7022c678
Panda-cool <pandacool.mrpanda@gmail.com>
2015-12-18 00:19:48 +0100

    Main clean

--- main.cpp
+++ main.cpp
@@ -23,63 +23,38 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 	else
-	{
+	{			
+		query = new Sequence(argv[1]); 
 			
-			const char* headerName = "/home/student/Documents/algo/uniprot_sprot.fasta.phr";
-
-			query = new Sequence(argv[1]); 
-			cout << query->getDescription() << endl;
-			cout << query->getChain() << endl;
-
-			db = new Database(argv[2], argv[3]);
-			cout << db->getTitle() << endl;
-			cout << db->getResiduesSize() << endl;
-			cout << db->getHeaderOffsets()[2] << endl;
-			cout << db->getSequences()[6] << endl;
-			
-			ifstream file(argv[4], ios::in);
-			if(file)
-			{
-				file.seekg(0, file.end);
-				int length = file.tellg();
-				file.seekg(0, file.beg);
-				buffer = new unsigned char[length];
-				file.read((char*)buffer, length);
-
-				Sequence *seq = new Sequence(buffer, db, 2);
-				cout << seq->getDescription() << endl;
-				cout << seq->getId() << endl;
-				cout << seq->getIdInfo() << endl;
-			}
-			if(argc > 5)
-				blosum = new Blosum(argv[5]);
-			else
-				blosum = new Blosum("BLOSUM62");
-
-			cout << blosum->getResidues() << endl;
-			cout << blosum->Matrix[0][0] << endl;
-			for(int j=0;j<24;j++)
-			{
-				for(int i=0;i<24;i++)
-					cout << blosum->Matrix[i][j] << " ";
-				cout << endl; 
-			}
-			
-		
-			if(argc > 6)
-				gapOpenPenalty = atoi(argv[6]);
-			else
-				gapOpenPenalty = 11;
-			cout << gapOpenPenalty << endl;
-			if(argc > 7)
-				gapExtensionPenalty = atoi(argv[7]);
-			else
-				gapExtensionPenalty = 1;
-			cout << gapExtensionPenalty << endl;
+		db = new Database(argv[2], argv[3]);
+	
+		ifstream file(argv[4], ios::in);
+		if(file)
+		{
+			file.seekg(0, file.end);
+			int length = file.tellg();
+			file.seekg(0, file.beg);
+			buffer = new unsigned char[length];
+			file.read((char*)buffer, length);
+		}
+		if(argc > 5)
+			blosum = new Blosum(argv[5]);
+		else
+			blosum = new Blosum("BLOSUM62");
+	
+		if(argc > 6)
+			gapOpenPenalty = atoi(argv[6]);
+		else
+			gapOpenPenalty = 11;
+		if(argc > 7)
+			gapExtensionPenalty = atoi(argv[7]);
+		else
+			gapExtensionPenalty = 1;
 
 		
 
 		//TODO: algorithme
+
+		return EXIT_SUCCESS; 
 	}
-	return EXIT_SUCCESS; 
 }
